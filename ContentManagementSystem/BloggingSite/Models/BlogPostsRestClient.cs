75e3b5a8133c093c7183e252bdf22146771f8560
Ashish <Ashish.Rathi@KLINGELNBERG.com>
2018-09-29 17:43:09 +0530

    Http client class added for calling a BlogPost api

---
+++ ContentManagementSystem/BloggingSite/Models/BlogPostsRestClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using Newtonsoft.Json;
+
+namespace BloggingSite.Models
+{
+    public class BlogPostsRestClient
+    {
+        private string BASE_URL = "https://localhost:44366/api/blogposts/";
+
+        public async Task<String> FindAll()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(BASE_URL);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var responseMessage = await client.GetAsync(BASE_URL);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return responseMessage.Content.ReadAsStringAsync().Result;
+            }
+            return null;
+        }
+    }
+}
