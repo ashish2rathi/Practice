f0bcef80623c315446ef0cd7e3a742240d857a51
Ashish <Ashish.Rathi@KLINGELNBERG.com>
2018-09-29 14:25:46 +0530

    Model and db context classes added for blog posts, make sure the property name and type match with column name and type in sql server

---
+++ ContentManagementSystem/DynamicPage.WebAPI/Models/BlogPosts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicPage.WebAPI.Models
+{
+    [Table("BlogPosts")]
+    public class BlogPosts
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string TopicID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime PublishedDate { get; set; }
+    }
+}
---
+++ ContentManagementSystem/DynamicPage.WebAPI/Models/BlogPostsDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DynamicPage.WebAPI.Models
+{
+    public class BlogPostsDBContext : DbContext
+    {
+        public DbSet<BlogPosts> BlogPosts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=KLIN0043\SQLEXPRESS;Initial Catalog=ContentManagementSystem;Integrated Security=True;");
+        }
+
+    }
+}
