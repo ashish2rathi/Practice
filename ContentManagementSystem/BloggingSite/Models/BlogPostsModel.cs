4dba06299a80249ac94d0f32454a9f3025804c41
Ashish <Ashish.Rathi@KLINGELNBERG.com>
2018-09-29 17:41:05 +0530

    Model class added for BlogPost in MVC application, to collect the result from API in this format.

---
+++ ContentManagementSystem/BloggingSite/Models/BlogPostsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloggingSite.Models
+{
+    public class BlogPostsModel
+    {
+        public int ID { get; set; }
+
+        public string TopicID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+    }
+}
